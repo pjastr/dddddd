4a7cfa92d529d0775e90d8c61465679db747c426
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-01 10:53:18 +0100

    Add files via upload

---
+++ ConsoleApplication59/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication59
+{
+    class Car:IComparable<Car>
+    {
+        private int year;
+        private string brand;
+
+        public Car(int year, string brand)
+        {
+            this.year = year;
+            this.brand = brand;
+        }
+
+        public int CompareTo(Car other)
+        {
+            return other.brand.CompareTo(this.brand);
+        }
+
+        public string Info()
+        {
+            return brand + " " + year;
+        }
+    }
+}
---
+++ ConsoleApplication59/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<int> liczby = new List<int>();
+            //liczby.Add(5);
+            //liczby.Add(-2);
+            //liczby.Add(3);
+            //liczby.Add(-10);
+            //liczby.Sort();
+            //foreach(var element in liczby)
+            //{
+            //    Console.WriteLine(element);
+            //}
+
+            List<Car> cars = new List<Car>();
+            cars.Add(new Car(2010, "Ford"));
+            cars.Add(new Car(2016, "Fiat"));
+            cars.Add(new Car(2012, "Skoda"));
+            cars.Add(new Car(2010, "BMW"));
+            cars.Sort();
+            foreach(var element in cars)
+            {
+                Console.WriteLine(element.Info());
+            }
+            Console.ReadKey();
+        }
+    }
+}
